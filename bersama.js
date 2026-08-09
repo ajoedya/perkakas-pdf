bb7c571ff845934089f12bddad8bcdbef59200d2
ajoedya <ajoedya@gmail.com>
2026-08-09 18:13:08 +0800

    Update bersama.js

--- bersama.js
+++ bersama.js
@@ -13,7 +13,8 @@
     { berkas: "nomor.html",     nama: "Penomor Halaman" },
     { berkas: "tanda-air.html", nama: "Watermark" },
     { berkas: "gambar.html",    nama: "PDF dan Gambar" },
-    { berkas: "penanda.html",   nama: "Penanda TTE" }
+    { berkas: "penanda.html",   nama: "Penanda TTE" },
+    { berkas: "kompres.html",   nama: "Kompres File PDF" }
   ];
 
   function pasangMenu() {
